fbc69ddbb439880bb5fae72d1336e873b316f3d9
mizuki-iwazaki <iwzkmzk@icloud.com>
2023-11-17 22:34:39 +0900

    トップページ作成 #フロント

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import AppIntrosection from './components/AppIntrosection';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AppIntrosection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
