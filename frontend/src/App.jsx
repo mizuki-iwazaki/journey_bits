d2918aa8a3d829624f191bc47edda5270ec4e152
mizuki-iwazaki <iwzkmzk@icloud.com>
2023-11-22 22:03:34 +0900

    ユーザー新規登録_フロント #4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import SignUp from './components/SignUp';
 import AppIntrosection from './components/AppIntrosection';
 import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AppIntrosection />
+      <Routes>
+        <Route exact path="/" element={<AppIntrosection />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
       <Footer />
     </div>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-gray-900 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container max-w-full flex justify-between items-center">
         <Link to="/" className="text-xl font-semibold">
-          <h1>Journey Bits</h1>
+          Journey Bits
         </Link>
         <nav>
           <ul className="flex">
             <li className="ml-4">
+              <Link to="/signup" className="text-xm">
+                新規登録
+              </Link>
+            </li>   
+            <li className="ml-4">
               {/* <Link to="/login">ログイン</Link> */}
               ログイン
-            </li>
-            <li className="ml-4">
-              {/* <Link to="/signup">新規登録</Link> */}
-              新規登録
-            </li>            
+            </li>         
           </ul>
         </nav>
       </div>
