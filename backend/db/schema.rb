5f4081d371e9861108bb604c4f25a5209e23d7cd
mizuki-iwazaki <iwzkmzk@icloud.com>
2023-11-24 23:53:10 +0900

    ユーザー登録修正

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,12 +12,13 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_19_120029) do
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
-    t.string "crypted_password", null: false
-    t.string "salt", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
