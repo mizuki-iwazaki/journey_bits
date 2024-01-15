708c0ffe15f45a76763907a20a91b4e6e07cbf2a
mizuki-iwazaki <iwzkmzk@icloud.com>
2024-01-15 23:00:08 +0900

    新規投稿の修正と項目の追加

--- backend/app/serializers/post_serializer.rb
+++ backend/app/serializers/post_serializer.rb
@@ -1,6 +1,16 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
+
   attributes :content, :status
+
+  attribute :image_urls do |post|
+    post.images.map do |image|
+      {
+        id: image.id,
+        url: image.image_file.url(:index_size)
+      }
+    end
+  end
 
   belongs_to :user
   belongs_to :theme
