aaa9b2c4e20ec1ce8fbd1ca244e5074e24e5ef31
mizuki-iwazaki <iwzkmzk@icloud.com>
2024-01-24 23:14:45 +0900

    いいね機能の実装

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :theme
   has_many :images, dependent: :destroy
+  has_many :likes
   has_one :location, dependent: :destroy
   accepts_nested_attributes_for :location
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_131857) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_125445) do
   create_table "api_keys", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "access_token", null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_131857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_images_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -72,6 +81,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_131857) do
 
   add_foreign_key "api_keys", "users"
   add_foreign_key "images", "posts"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "locations", "posts"
   add_foreign_key "posts", "themes"
   add_foreign_key "posts", "users"
