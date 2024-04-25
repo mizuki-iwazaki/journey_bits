c3414f08fd87406ae72cd16b27dad5aef005b24a
mizuki-iwazaki <iwzkmzk@icloud.com>
2024-04-25 23:29:04 +0900

    API認証の再設定

--- backend/app/jobs/image_analyses_job.rb
+++ backend/app/jobs/image_analyses_job.rb
@@ -4,13 +4,13 @@ class ImageAnalysesJob < ApplicationJob
   queue_as :default
 
   def perform(post_id)
-    # 環境変数から認証情報を自動的に読み込む
+    post = Post.find(post_id)
+
     vision = Google::Cloud::Vision.image_annotator
 
-    post = Post.find(post_id)
     post.images.each do |image|
-      # 画像解析を行う
-      response = vision.label_detection image: image.image_file.url, max_results: 2
+      # CloudinaryのURLを直接Google Cloud Vision APIに渡す
+      response = vision.label_detection(image: image.image_file.url, max_results: 2)
 
       # 解析結果を取得
       labels = response.responses.map(&:label_annotations).flatten
@@ -20,10 +20,12 @@ class ImageAnalysesJob < ApplicationJob
       AnalysisResult.create!(
         post: post,
         image: image,
-        detected_labels: labels_data,
+        detected_labels: labels_data
       )
     end
   rescue Google::Cloud::Error => e
-    Rails.logger.error "Google Cloud Vision APIエラー: #{e.message}"
+    Rails.logger.error "Google Cloud Vision API Error: #{e.message}"
+  rescue StandardError => e
+    Rails.logger.error "Error in ImageAnalysesJob: #{e.message}"
   end
 end
